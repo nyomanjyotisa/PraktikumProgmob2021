175aa08a536e2d1f7ac4de5123978038724431ea
nyomanjyotisa <jyotisa1616@gmail.com>
2021-10-05 16:13:24 +0800

    Initial commit

---
+++ app/src/main/java/id/jyotisa/praktikumprogmob2021/MainActivity.java
@@ -0,0 +1,14 @@
+package id.jyotisa.praktikumprogmob2021;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}

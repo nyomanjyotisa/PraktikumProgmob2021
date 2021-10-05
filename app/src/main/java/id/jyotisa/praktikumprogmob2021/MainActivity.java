49f7284e4643732cb49b27492a1b028bc1ad0c0f
nyomanjyotisa <jyotisa1616@gmail.com>
2021-10-05 22:21:32 +0800

    bottom nav & some fragments

--- app/src/main/java/id/jyotisa/praktikumprogmob2021/MainActivity.java
+++ app/src/main/java/id/jyotisa/praktikumprogmob2021/MainActivity.java
@@ -1,14 +1,47 @@
 package id.jyotisa.praktikumprogmob2021;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
 
+import com.ismaeldivita.chipnavigation.ChipNavigationBar;
+
 public class MainActivity extends AppCompatActivity {
+
+    private ChipNavigationBar chipNavigationBar;
+    private Fragment fragment = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        chipNavigationBar = findViewById(R.id.chipNav);
+
+        chipNavigationBar.setItemSelected(R.id.home, true);
+        getSupportFragmentManager().beginTransaction().replace(R.id.container, new HomeFragment()).commit();
+
+        chipNavigationBar.setOnItemSelectedListener(new ChipNavigationBar.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(int i) {
+                switch (i) {
+                    case R.id.home:
+                        fragment = new HomeFragment();
+                        break;
+                    case R.id.explore:
+                        fragment = new ExploreFragment();
+                        break;
+                    case R.id.newJob:
+                        fragment = new NewFragment();
+                        break;
+
+                }
+
+                if (fragment != null) {
+                    getSupportFragmentManager().beginTransaction().replace(R.id.container, fragment).commit();
+                }
+            }
+        });
     }
 }

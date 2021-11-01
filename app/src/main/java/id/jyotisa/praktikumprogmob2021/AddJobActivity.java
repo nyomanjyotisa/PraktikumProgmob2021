c0710a9cc332f8d18bc464b657d2637633b3d460
nyomanjyotisa <jyotisa1616@gmail.com>
2021-11-01 13:15:19 +0800

    clean up code

--- app/src/main/java/id/jyotisa/praktikumprogmob2021/AddJobActivity.java
+++ app/src/main/java/id/jyotisa/praktikumprogmob2021/AddJobActivity.java
@@ -17,45 +17,43 @@ import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.SeekBar;
 import android.widget.TextView;
-
+import android.widget.Toast;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.amulyakhare.textdrawable.TextDrawable;
 import com.amulyakhare.textdrawable.util.ColorGenerator;
 import com.google.android.material.textfield.TextInputLayout;
-
-import java.text.DecimalFormat;
 import java.text.NumberFormat;
 import java.util.ArrayList;
 import java.util.Locale;
 
+import es.dmoral.toasty.Toasty;
 import id.jyotisa.praktikumprogmob2021.model.Job;
 
 public class AddJobActivity extends AppCompatActivity {
 
     private Button btnSubmit;
-    AlertDialog.Builder dialog;
-    LayoutInflater inflater;
-    View dialogView;
-    TextInputLayout etCompanyName, etJobTitle, etJobDesc, etLocation;
-    EditText etSalary;
-    ArrayList<String> benefits;
-    TextView tvCompanyName, tvJobTitle, tvJobDesc, tvLocation, tvSalary, tvJobType, tvBenefits;
-    RadioGroup radioJobType;
-    CheckBox benefit1, benefit2, benefit3, benefit4;
-    RadioButton radioButtonJobType;
-    SeekBar seekBarSalary;
+    private AlertDialog.Builder dialog;
+    private LayoutInflater inflater;
+    private View dialogView;
+    private TextInputLayout etCompanyName, etJobTitle, etJobDesc, etLocation;
+    private EditText etSalary;
+    private ArrayList<String> benefits;
+    private TextView tvCompanyName, tvJobTitle, tvJobDesc, tvLocation, tvSalary, tvJobType, tvBenefits;
+    private RadioGroup radioJobType;
+    private CheckBox benefit1, benefit2, benefit3, benefit4;
+    private RadioButton radioButtonJobType;
+    private SeekBar seekBarSalary;
+    private String formattedSalary;
+    private StringBuilder stringBenefits;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setTheme(R.style.Theme_PraktikumProgmob2021);
         setContentView(R.layout.activity_add);
-
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        getSupportActionBar().setBackgroundDrawable(new ColorDrawable(Color.parseColor("#ffffff")));
-        getSupportActionBar().setTitle("Create a Job");
+        setCustomActionBar();
 
         seekBarSalary = findViewById(R.id.salarySeekBar);
         etCompanyName = findViewById(R.id.companyNameEditText);
@@ -64,136 +62,25 @@ public class AddJobActivity extends AppCompatActivity {
         etLocation = findViewById(R.id.locationEditText);
         etSalary = findViewById(R.id.salaryEditText);
 
-        Integer companyNameLimit = 30;
-        Integer jobTitleLimit = 50;
-        Integer jobDescLimit = 1000;
-        Integer locationLimit = 15;
-
-        onEditValidationInput(etCompanyName, companyNameLimit);
-        onEditValidationInput(etJobTitle, jobTitleLimit);
-        onEditValidationInput(etJobDesc, jobDescLimit);
-        onEditValidationInput(etLocation, locationLimit);
-
-        etCompanyName.getEditText().addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-
-            }
-
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-
-            }
-
-            @Override
-            public void afterTextChanged(Editable s) {
-
-            }
-        });
-
-        etSalary.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-
-            }
-
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-                if(etSalary.getText().toString().trim().length() > 0) {
-                    seekBarSalary.setProgress(Integer.parseInt(etSalary.getText().toString()));
-                    etSalary.setSelection(etSalary.getText().length());
-                }
-            }
-
-            @Override
-            public void afterTextChanged(Editable s) {
-
-            }
-        });
-
-        seekBarSalary.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
-            @Override
-            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                etSalary.setText(String.valueOf(progress));
-            }
-
-            @Override
-            public void onStartTrackingTouch(SeekBar seekBar) {
-
-            }
-
-            @Override
-            public void onStopTrackingTouch(SeekBar seekBar) {
-
-            }
-        });
+        formValidation("onEdit");
+        seekBarAndEditTextIntegration();
 
         benefit1 = (CheckBox)findViewById(R.id.benefit1);
         benefit2 = (CheckBox)findViewById(R.id.benefit2);
         benefit3 = (CheckBox)findViewById(R.id.benefit3);
         benefit4 = (CheckBox)findViewById(R.id.benefit4);
         benefits = new ArrayList<>();
-
-        benefit1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (benefit1.isChecked())
-                    benefits.add(benefit1.getText().toString());
-                else
-                    benefits.remove(benefit1.getText().toString());
-            }
-        });
-
-        benefit2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (benefit2.isChecked())
-                    benefits.add(benefit2.getText().toString());
-                else
-                    benefits.remove(benefit2.getText().toString());
-            }
-        });
-
-        benefit3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (benefit3.isChecked())
-                    benefits.add(benefit3.getText().toString());
-                else
-                    benefits.remove(benefit3.getText().toString());
-            }
-        });
-
-        benefit4.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (benefit4.isChecked())
-                    benefits.add(benefit4.getText().toString());
-                else
-                    benefits.remove(benefit4.getText().toString());
-            }
-        });
+        benefitOnClick();
 
         btnSubmit = (Button) findViewById(R.id.createButton);
-
         btnSubmit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                boolean valid = true;
-                if(!onSubmitValidationInput(etCompanyName, companyNameLimit))
-                    valid = false;
-
-                if(!onSubmitValidationInput(etJobTitle, jobTitleLimit))
-                    valid = false;
-
-                if(!onSubmitValidationInput(etJobDesc, jobDescLimit))
-                    valid = false;
-
-                if(!onSubmitValidationInput(etLocation, locationLimit))
-                    valid = false;
-
-                if(valid)
+                if(formValidation("onSubmit")){
                     showDialog(benefits);
+                }else{
+                    Toasty.error(AddJobActivity.this, "Some fields contain error", Toast.LENGTH_SHORT, true).show();
+                }
             }
         });
     }
@@ -203,29 +90,32 @@ public class AddJobActivity extends AppCompatActivity {
         inflater = getLayoutInflater();
         dialogView = inflater.inflate(R.layout.form_data, null);
         dialog.setView(dialogView);
-
         dialog.setTitle("Job Preview");
 
-        radioJobType = findViewById(R.id.jobTypeRadio);
-
-        int selectedId = radioJobType.getCheckedRadioButtonId();
-        radioButtonJobType = (RadioButton) findViewById(selectedId);
-
-        StringBuilder stringBenefits = new StringBuilder();
+        //benefits array to string
+        stringBenefits = new StringBuilder();
         for (String s : benefits)
             stringBenefits.append(" - "+s).append("\n");
 
-        etCompanyName = findViewById(R.id.companyNameEditText);
-        etJobTitle = findViewById(R.id.jobTitleEditText);
-        etJobDesc = findViewById(R.id.jobDescEditText);
-        etLocation = findViewById(R.id.locationEditText);
-        etSalary = findViewById(R.id.salaryEditText);
+        //set format for salary
+        int salary;
+        if(etSalary.getText().toString().isEmpty()){
+            salary = 0;
+        }else{
+            salary = Integer.parseInt(etSalary.getText().toString());
+        }
+        formattedSalary = NumberFormat.getNumberInstance(Locale.US).format(salary);
+
+        //jobtype
+        radioJobType = findViewById(R.id.jobTypeRadio);
+        int selectedId = radioJobType.getCheckedRadioButtonId();
+        radioButtonJobType = (RadioButton) findViewById(selectedId);
 
         tvCompanyName = (TextView) dialogView.findViewById(R.id.companyName);
-        tvJobTitle = (TextView) dialogView.findViewById(R.id.jobTitle);
+        tvJobTitle = (TextView) dialogView.findViewById(R.id.jobName);
         tvJobDesc = (TextView) dialogView.findViewById(R.id.jobDesc);
         tvLocation = (TextView) dialogView.findViewById(R.id.location);
-        tvSalary = (TextView) dialogView.findViewById(R.id.salary);
+        tvSalary = (TextView) dialogView.findViewById(R.id.jobSalary);
         tvJobType = (TextView) dialogView.findViewById(R.id.jobType);
         tvBenefits = (TextView) dialogView.findViewById(R.id.benefits);
 
@@ -233,31 +123,11 @@ public class AddJobActivity extends AppCompatActivity {
         tvJobTitle.setText(etJobTitle.getEditText().getText().toString());
         tvJobDesc.setText(etJobDesc.getEditText().getText().toString());
         tvLocation.setText(etLocation.getEditText().getText().toString());
-
-        ColorGenerator generator = ColorGenerator.MATERIAL;
-        int color2 = generator.getColor(String.valueOf(etCompanyName.getEditText().getText().toString().charAt(0)));
-
-        TextDrawable drawable = TextDrawable.builder()
-                .beginConfig()
-                .bold()
-                .endConfig()
-                .buildRound(String.valueOf(etCompanyName.getEditText().getText().toString().charAt(0)), color2);
-
-        ImageView image = (ImageView) dialogView.findViewById(R.id.logo);
-        image.setImageDrawable(drawable);
-
-        Integer salary;
-
-        if(etSalary.getText().toString().isEmpty()){
-            salary = 0;
-        }else{
-            salary = Integer.parseInt(etSalary.getText().toString());
-        }
-
-        String formattedSalary = NumberFormat.getNumberInstance(Locale.US).format(salary);
         tvSalary.setText("Salary: $"+ formattedSalary  +"/month");
         tvJobType.setText(radioButtonJobType.getText());
         tvBenefits.setText(stringBenefits);
+
+        setTextDrawable(etCompanyName.getEditText().getText().toString(), dialogView);
 
         dialog.setPositiveButton("POST", new DialogInterface.OnClickListener() {
             @Override
@@ -320,5 +190,138 @@ public class AddJobActivity extends AppCompatActivity {
         }else{
             return true;
         }
+    }
+
+    private void benefitOnClick() {
+        benefit1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (benefit1.isChecked())
+                    benefits.add(benefit1.getText().toString());
+                else
+                    benefits.remove(benefit1.getText().toString());
+            }
+        });
+
+        benefit2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (benefit2.isChecked())
+                    benefits.add(benefit2.getText().toString());
+                else
+                    benefits.remove(benefit2.getText().toString());
+            }
+        });
+
+        benefit3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (benefit3.isChecked())
+                    benefits.add(benefit3.getText().toString());
+                else
+                    benefits.remove(benefit3.getText().toString());
+            }
+        });
+
+        benefit4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (benefit4.isChecked())
+                    benefits.add(benefit4.getText().toString());
+                else
+                    benefits.remove(benefit4.getText().toString());
+            }
+        });
+    }
+
+    private void seekBarAndEditTextIntegration(){
+        etSalary.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if(etSalary.getText().toString().trim().length() > 0) {
+                    seekBarSalary.setProgress(Integer.parseInt(etSalary.getText().toString()));
+                    etSalary.setSelection(etSalary.getText().length());
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+        seekBarSalary.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                etSalary.setText(String.valueOf(progress));
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+    }
+
+    private boolean formValidation(String when) {
+        Integer companyNameLimit = 30;
+        Integer jobTitleLimit = 50;
+        Integer jobDescLimit = 1000;
+        Integer locationLimit = 15;
+
+        if(when.equals("onEdit")){
+            onEditValidationInput(etCompanyName, companyNameLimit);
+            onEditValidationInput(etJobTitle, jobTitleLimit);
+            onEditValidationInput(etJobDesc, jobDescLimit);
+            onEditValidationInput(etLocation, locationLimit);
+        }else if(when.equals("onSubmit")){
+            boolean valid = true;
+            if(!onSubmitValidationInput(etCompanyName, companyNameLimit))
+                valid = false;
+
+            if(!onSubmitValidationInput(etJobTitle, jobTitleLimit))
+                valid = false;
+
+            if(!onSubmitValidationInput(etJobDesc, jobDescLimit))
+                valid = false;
+
+            if(!onSubmitValidationInput(etLocation, locationLimit))
+                valid = false;
+
+            if(!valid)
+                return false;
+        }
+
+        return true;
+    }
+
+    private void setCustomActionBar(){
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setBackgroundDrawable(new ColorDrawable(Color.parseColor("#ffffff")));
+        getSupportActionBar().setTitle("Create a Job");
+    }
+
+    private void setTextDrawable(String companyName, View view){
+        ColorGenerator generator = ColorGenerator.MATERIAL;
+        int color2 = generator.getColor(String.valueOf(companyName.charAt(0)));
+
+        TextDrawable drawable = TextDrawable.builder()
+                .beginConfig()
+                .bold()
+                .endConfig()
+                .buildRound(String.valueOf(companyName.charAt(0)), color2);
+
+        ImageView image = (ImageView) view.findViewById(R.id.logo);
+        image.setImageDrawable(drawable);
     }
 }
--- app/src/main/java/id/jyotisa/praktikumprogmob2021/DetailActivity.java
+++ app/src/main/java/id/jyotisa/praktikumprogmob2021/DetailActivity.java
@@ -28,44 +28,23 @@ import com.amulyakhare.textdrawable.util.ColorGenerator;
 
 import java.util.zip.Inflater;
 
+import es.dmoral.toasty.Toasty;
+import id.jyotisa.praktikumprogmob2021.helper.CompanyTextDrawable;
 import id.jyotisa.praktikumprogmob2021.model.Job;
 
 public class DetailActivity extends AppCompatActivity {
-
-    TextView tvJobTitle, tvJobDesc, tvLocation, tvSalary, tvJobType, tvBenefits;
+    TextView tvJobTitle, tvJobDesc, tvLocation, tvSalary, tvJobType, tvBenefits, tvCompanyName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setTheme(R.style.Theme_PraktikumProgmob2021);
         setContentView(R.layout.activity_detail);
+        setCustomActionBar();
 
         Job job = getIntent().getParcelableExtra("JOB");
 
-        LayoutInflater layoutInflater = (LayoutInflater)this.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        View view = layoutInflater.inflate(R.layout.title_format, null);
-        TextView titleActionBar = (TextView) view.findViewById(R.id.titleActionBar);
-        titleActionBar.setText(job.getCompanyName());
-        titleActionBar.setGravity(Gravity.CENTER);
-
-        getSupportActionBar().setTitle(job.getCompanyName());
-        getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
-        getSupportActionBar().setCustomView(view);
-        getSupportActionBar().setBackgroundDrawable(new ColorDrawable(Color.parseColor("#ffffff")));
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-
-        ColorGenerator generator = ColorGenerator.MATERIAL;
-        int color2 = generator.getColor(String.valueOf(job.getCompanyName().charAt(0)));
-
-        TextDrawable drawable = TextDrawable.builder()
-                .beginConfig()
-                    .bold()
-                .endConfig()
-                .buildRound(String.valueOf(job.getCompanyName().charAt(0)), color2);
-
-        ImageView image = (ImageView) findViewById(R.id.logo);
-        image.setImageDrawable(drawable);
-
+        tvCompanyName = (TextView) findViewById(R.id.companyName);
         tvJobTitle = (TextView) findViewById(R.id.jobName);
         tvJobDesc = (TextView) findViewById(R.id.jobDesc);
         tvLocation = (TextView) findViewById(R.id.location);
@@ -73,12 +52,15 @@ public class DetailActivity extends AppCompatActivity {
         tvJobType = (TextView) findViewById(R.id.jobType);
         tvBenefits = (TextView) findViewById(R.id.benefits);
 
+        tvCompanyName.setText(job.getCompanyName());
         tvJobTitle.setText(job.getJobTitle());
         tvJobDesc.setText(job.getJobDesc());
         tvLocation.setText(job.getLocation());
         tvJobType.setText(job.getJobType());
         tvSalary.setText("Salary: $"+ job.getSalary()  +"/month");
         tvBenefits.setText(job.getBenefits());
+
+        setTextDrawable(job.getCompanyName());
     }
 
     @Override
@@ -103,16 +85,15 @@ public class DetailActivity extends AppCompatActivity {
     }
 
     @Override
-    protected void onDestroy() {
-        super.onDestroy();
-
-        Toast.makeText(DetailActivity.this, "You are leaving detail page", Toast.LENGTH_SHORT).show();
+    protected void onStop() {
+        super.onStop();
+        Toasty.info(DetailActivity.this, "Your job detail page no longer visible", Toast.LENGTH_SHORT, true).show();
     }
 
     @Override
-    protected void onStop() {
-        super.onStop();
-        Toast.makeText(DetailActivity.this, "Your job detail page no longer visible", Toast.LENGTH_SHORT).show();
+    protected void onDestroy() {
+        super.onDestroy();
+        Toasty.warning(DetailActivity.this, "You are leaving detail page", Toast.LENGTH_SHORT, true).show();
     }
 
     @Override
@@ -120,5 +101,25 @@ public class DetailActivity extends AppCompatActivity {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.owner_detail_menu, menu);
         return true;
+    }
+
+    private void setTextDrawable(String companyName){
+        ColorGenerator generator = ColorGenerator.MATERIAL;
+        int color2 = generator.getColor(String.valueOf(companyName.charAt(0)));
+
+        TextDrawable drawable = TextDrawable.builder()
+                .beginConfig()
+                .bold()
+                .endConfig()
+                .buildRound(String.valueOf(companyName.charAt(0)), color2);
+
+        ImageView image = (ImageView) findViewById(R.id.logo);
+        image.setImageDrawable(drawable);
+    }
+
+    private void setCustomActionBar(){
+        getSupportActionBar().setTitle("Job Details");
+        getSupportActionBar().setBackgroundDrawable(new ColorDrawable(Color.parseColor("#ffffff")));
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 }
